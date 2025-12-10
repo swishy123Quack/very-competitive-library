b8fef0176e5cb27f05c364fa209fc3c4cc67b3d4
lenhanbo <88267031+lenhanbo@users.noreply.github.com>
2025-12-11 00:30:52 +0700

    Update and rename CRT.md to CRT.cpp

---
+++ lenhanbo/CRT.cpp
@@ -0,0 +1,12 @@
+//When calculate with modulo m is not prime. Factorzie m to prime numbers and calculate solution on these prime numbers. Then use CRT to find the final answer
+
+pair<ll, ll> CRT(ll x1, ll m1, ll x2, ll m2) { // repeatively do this on all modulo to get final result
+  ll g = gcd(m1, m2);
+  if ((x2 - x1) % g) return make_pair(-1, -1);// no sol
+  m1 /= g, m2 /= g;
+  pair<ll, ll> p = extgcd(m1, m2);
+  ll lcm = m1 * m2 * g;
+  ll res = p.first * (x2 - x1) * m1 + x1;
+  // be careful with overflow
+  return make_pair((res % lcm + lcm) % lcm, lcm); // return x mod (lcm(m1,m2,...mi))
+}
--- lenhanbo/CRT.md
+++
@@ -1,2 +0,0 @@
-When calculate with modulo m is not prime. Factorzie m to prime numbers and calculate solution on these prime numbers. Then use CRT to find the final answer
-
