c3f677a9f29576189a2613ce36cca63793334627
lenhanbo <88267031+lenhanbo@users.noreply.github.com>
2026-01-09 12:44:45 +0700

    Update mobius.cpp

--- lenhanbo/mobius.cpp
+++ lenhanbo/mobius.cpp
@@ -10,3 +10,5 @@ void mobcalc(){ // calculate in nlog
 }
 
 //ta co sum(mob[d | n]) = [n == 1] (n co bang 1 hay ko) nen tim cach bien doi cong thuc ve dang nay
+// sum gcd(i,j) = sum(x) sum(x | i) sum(x | j) [gcd(i/x, j/x) == 1] = sum(x) 
+// sau do thay  [gcd(i/x, j/x) == 1] = sum(d|gcd(i/x, j/x)) mob[d], sau do tim cach dua d ra ngoai hoac co the dat T = d*x roi xu ly
